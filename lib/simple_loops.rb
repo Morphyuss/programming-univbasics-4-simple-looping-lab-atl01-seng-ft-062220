04ac8d43de0e24e17539edda7ea856a018a5623b
James Stamps Jr <jstampshere@gmail.com>
2020-04-05 21:26:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,7 +29,14 @@ def output_array(array)
 end
   
   
+def reutrn_string_array(array)
+  counter = 0
   
+  while counter < array.length do
+    puts array.to_s
+  counter +=1
+end
+end
   
   
   
