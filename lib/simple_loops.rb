1c5b514557744d7b186d8e34cb06dafc4c2e6ddd
James Stamps Jr <jstampshere@gmail.com>
2020-04-05 21:17:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,3 +18,28 @@ def loop_message_n_times(string, integer)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0
+  
+  while counter < array.length do
+    puts string
+    counter +=1
+  end
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
